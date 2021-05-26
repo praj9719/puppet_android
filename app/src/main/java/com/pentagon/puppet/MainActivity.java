312085bfe48f25f3bca0dc74940f62469cd96207
Prajwal <prajwaljadhav248@gmail.com>
2021-05-26 23:39:19 +0530

    dummy splash screen and homeactivity added

--- app/src/main/java/com/pentagon/puppet/MainActivity.java
+++ app/src/main/java/com/pentagon/puppet/MainActivity.java
@@ -2,13 +2,25 @@ package com.pentagon.puppet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(MainActivity.this, HomeActivity.class));
+        }, 1000);
     }
 }
